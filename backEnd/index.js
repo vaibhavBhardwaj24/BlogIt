4dc91d6abe5f52ce9976f98c692af0bbbb6aff8b
vaibhavBhardwaj24 <bhardwajvaibhav2412@gmail.com>
2024-06-26 19:44:50 +0530

    without middleware

--- backEnd/index.js
+++ backEnd/index.js
@@ -17,17 +17,17 @@ app.use("", (req, res) => {
   res.json({ message: "sdrty" });
 });
 dotenv.config({ path: "src/.env" });
-app.use(
-  cors({
-    origin: "*",
-    credentials: true,
-    allowedHeaders: ["Authorization", "Content-Type"],
-  })
-);
-app.use(express.json({ limit: "32kb" }));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
-app.use(cookieParser());
+// app.use(
+//   cors({
+//     origin: "*",
+//     credentials: true,
+//     allowedHeaders: ["Authorization", "Content-Type"],
+//   })
+// );
+// app.use(express.json({ limit: "32kb" }));
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.static("public"));
+// app.use(cookieParser());
 app.use("/v1/user", userRouter);
 app.use("/v1/f", followRouter);
 app.use("/v1/b", blogRouter);
@@ -36,9 +36,8 @@ app.use("/v1/l", likedRouter);
 app.use("/v1/s", savedRouter);
 console.log("starting");
 connectDB().then(() => {
-  // app.listen(3000, () => {
-  //   console.log("listening");
-  // });
-  console.log("listening");
+  app.listen(3000, () => {
+    console.log("listening");
+  });
 });
 export const handler=serverless(app)
