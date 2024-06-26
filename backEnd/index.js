6afbe522cf461d96e32d0b1eed4b3d3679177047
vaibhavBhardwaj24 <bhardwajvaibhav2412@gmail.com>
2024-06-26 19:49:42 +0530

    pls

--- backEnd/index.js
+++ backEnd/index.js
@@ -1,43 +1,36 @@
 import dotenv from "dotenv";
 // import { default as connectDB } from "";
 import { default as connectDB } from "./src/utils/db.js";
-
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import userRouter from "./src/router/user.router.js";
-import followRouter from "./src/router/follow.router.js";
-import blogRouter from "./src/router/blogs.router.js";
-import commentRouter from "./src/router/comment.router.js";
-import likedRouter from "./src/router/liked.route.js";
-import savedRouter from "./src/router/saved.router.js";
+// import { app } from "./app.js";
+// import { app } from "./app.js";
 import express from "express";
-const app = express();
+const app=express()
 import serverless from "serverless-http";
-app.use("", (req, res) => {
-  res.json({ message: "sdrty" });
-});
-// dotenv.config({ path: "src/.env" });
-// app.use(
-//   cors({
-//     origin: "*",
-//     credentials: true,
-//     allowedHeaders: ["Authorization", "Content-Type"],
-//   })
-// );
-// app.use(express.json({ limit: "32kb" }));
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.static("public"));
-// app.use(cookieParser());
-// app.use("/v1/user", userRouter);
-// app.use("/v1/f", followRouter);
-// app.use("/v1/b", blogRouter);
-// app.use("/v1/c", commentRouter);
-// app.use("/v1/l", likedRouter);
-// app.use("/v1/s", savedRouter);
+app.use("",(req,res)=>{
+  res.json({message:"sdrty"})
+})
+dotenv.config({ path: "src/.env" });
 console.log("starting");
-connectDB().then(() => {
-  app.listen(3000, () => {
-    console.log("listening");
+// const handler = async (req, res) => {
+  connectDB().then(() => {
+    app.listen(3000, () => {
+      console.log("listening");
+    });
   });
-});
-// export const handler=serverless(app)
+  
+// };
+// import dotenv from "dotenv";
+
+// dotenv.config({ path: "src/.env" });
+
+// let isConnected = false;
+
+// const handler = async (req, res) => {
+//   if (!isConnected) {
+//     await connectDB();
+//     isConnected = true;
+//   }
+//   return serverless(app)(req, res);
+// };
+
+// export default handler;
