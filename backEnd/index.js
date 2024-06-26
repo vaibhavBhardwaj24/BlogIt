5021440dbabf265c4c24b120fccabca5a48684da
vaibhavBhardwaj24 <bhardwajvaibhav2412@gmail.com>
2024-06-26 19:46:19 +0530

    without routes

--- backEnd/index.js
+++ backEnd/index.js
@@ -28,16 +28,16 @@ dotenv.config({ path: "src/.env" });
 // app.use(express.urlencoded({ extended: true }));
 // app.use(express.static("public"));
 // app.use(cookieParser());
-app.use("/v1/user", userRouter);
-app.use("/v1/f", followRouter);
-app.use("/v1/b", blogRouter);
-app.use("/v1/c", commentRouter);
-app.use("/v1/l", likedRouter);
-app.use("/v1/s", savedRouter);
+// app.use("/v1/user", userRouter);
+// app.use("/v1/f", followRouter);
+// app.use("/v1/b", blogRouter);
+// app.use("/v1/c", commentRouter);
+// app.use("/v1/l", likedRouter);
+// app.use("/v1/s", savedRouter);
 console.log("starting");
 connectDB().then(() => {
   app.listen(3000, () => {
     console.log("listening");
   });
 });
-export const handler=serverless(app)
+// export const handler=serverless(app)
