f54735c47fa2ff66df05e4755f664e567a05932e
vaibhavBhardwaj24 <bhardwajvaibhav2412@gmail.com>
2024-06-26 19:36:03 +0530

    #

--- backEnd/app.js
+++ backEnd/app.js
@@ -8,27 +8,27 @@ import commentRouter from "./src/router/comment.router.js";
 import likedRouter from "./src/router/liked.route.js";
 import savedRouter from "./src/router/saved.router.js";
 const app = express();
-// app.use(
-//   cors(
-//     {
-//       // origin: ["http://localhost:5173"," https://db1e-182-69-182-255.ngrok-free.app"],
-//       origin:"*",
-//       credentials: true,
-//       allowedHeaders: ["Authorization", "Content-Type"],
-//     },
+app.use(
+  cors(
+    {
+      // origin: ["http://localhost:5173"," https://db1e-182-69-182-255.ngrok-free.app"],
+      origin:"*",
+      credentials: true,
+      allowedHeaders: ["Authorization", "Content-Type"],
+    },
 
-//   )
-// );
-// app.use(express.json({ limit: "32kb" }));
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.static("public"));
-// app.use(cookieParser());
-// app.use("/v1/user", userRouter);
-// app.use("/v1/f", followRouter);
-// app.use("/v1/b", blogRouter);
-// app.use("/v1/c", commentRouter);
-// app.use("/v1/l", likedRouter);
-// app.use("/v1/s", savedRouter);
+  )
+);
+app.use(express.json({ limit: "32kb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(cookieParser());
+app.use("/v1/user", userRouter);
+app.use("/v1/f", followRouter);
+app.use("/v1/b", blogRouter);
+app.use("/v1/c", commentRouter);
+app.use("/v1/l", likedRouter);
+app.use("/v1/s", savedRouter);
 app.use("/", (req, res) => {
   res.status(200).send("Hello from Express!");
 });
--- backEnd/index.js
+++ backEnd/index.js
@@ -1,23 +1,47 @@
 import dotenv from "dotenv";
 // import { default as connectDB } from "";
 import { default as connectDB } from "./src/utils/db.js";
-// import { app } from "./app.js";
-// import { app } from "./app.js";
+
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import userRouter from "./src/router/user.router.js";
+import followRouter from "./src/router/follow.router.js";
+import blogRouter from "./src/router/blogs.router.js";
+import commentRouter from "./src/router/comment.router.js";
+import likedRouter from "./src/router/liked.route.js";
+import savedRouter from "./src/router/saved.router.js";
 import express from "express";
-const app=express()
+const app = express();
 import serverless from "serverless-http";
-app.use("",(req,res)=>{
-  res.json({message:"sdrty"})
-})
+app.use("", (req, res) => {
+  res.json({ message: "sdrty" });
+});
 dotenv.config({ path: "src/.env" });
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+    allowedHeaders: ["Authorization", "Content-Type"],
+  })
+);
+app.use(express.json({ limit: "32kb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(cookieParser());
+app.use("/v1/user", userRouter);
+app.use("/v1/f", followRouter);
+app.use("/v1/b", blogRouter);
+app.use("/v1/c", commentRouter);
+app.use("/v1/l", likedRouter);
+app.use("/v1/s", savedRouter);
 console.log("starting");
 // const handler = async (req, res) => {
-  connectDB().then(() => {
-    app.listen(3000, () => {
-      console.log("listening");
-    });
+connectDB().then(() => {
+  app.listen(3000, () => {
+    console.log("listening");
   });
-  
+});
+
 // };
 // import dotenv from "dotenv";
 
