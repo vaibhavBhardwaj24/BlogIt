5141397477e437cbb91b73eda110a15c07e2bcdd
vaibhavBhardwaj24 <bhardwajvaibhav2412@gmail.com>
2024-06-26 19:32:45 +0530

    #

--- backEnd/index.js
+++ backEnd/index.js
@@ -2,19 +2,20 @@ import dotenv from "dotenv";
 // import { default as connectDB } from "";
 import { default as connectDB } from "./src/utils/db.js";
 // import { app } from "./app.js";
-import { app } from "./app.js";
+// import { app } from "./app.js";
+import express from "express";
+const app=express()
 import serverless from "serverless-http";
 dotenv.config({ path: "src/.env" });
 console.log("starting");
-const handler = async (req, res) => {
+// const handler = async (req, res) => {
   connectDB().then(() => {
     app.listen(3000, () => {
       console.log("listening");
     });
   });
-  return serverless(app)(req,res);
-};
-export default handler
+  
+// };
 // import dotenv from "dotenv";
 
 // dotenv.config({ path: "src/.env" });
