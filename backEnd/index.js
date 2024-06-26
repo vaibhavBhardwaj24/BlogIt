f2fad1cefd37990d8b9b3ef8552a5daa402bdbd6
vaibhavBhardwaj24 <bhardwajvaibhav2412@gmail.com>
2024-06-26 19:23:55 +0530

    ##

--- backEnd/index.js
+++ backEnd/index.js
@@ -1,28 +1,32 @@
-// import dotenv from "dotenv";
-// // import { default as connectDB } from "";
-import {default as connectDB} from "./src/utils/db.js"
-import { app } from "./app.js";
-// import { app } from "./app.js";
-// dotenv.config({ path: "src/.env" });
-// console.log("starting");
-// connectDB().then(() => {
-//   app.listen(process.env.PORT, () => {
-//     console.log("listening");
-//   });
-// });
 import dotenv from "dotenv";
+// import { default as connectDB } from "";
+import { default as connectDB } from "./src/utils/db.js";
+// import { app } from "./app.js";
+import { app } from "./app.js";
 import serverless from "serverless-http";
-
 dotenv.config({ path: "src/.env" });
-
-let isConnected = false;
-
+console.log("starting");
 const handler = async (req, res) => {
-  if (!isConnected) {
-    await connectDB();
-    isConnected = true;
-  }
-  return serverless(app)(req, res);
+  connectDB().then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("listening");
+    });
+  });
+  return serverless(app)
 };
+export default handler
+// import dotenv from "dotenv";
 
-export default handler;
+// dotenv.config({ path: "src/.env" });
+
+// let isConnected = false;
+
+// const handler = async (req, res) => {
+//   if (!isConnected) {
+//     await connectDB();
+//     isConnected = true;
+//   }
+//   return serverless(app)(req, res);
+// };
+
+// export default handler;
